f111b1900e97351d36e31f80b21c3686a3244b0f
Crowdbotics <engineering@crowdbotics.com>
2023-06-24 23:13:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-water-dev-94414",
+  url: "https://black-water-dev-94414.botics.co",
+  api: "https://black-water-dev-94414.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
